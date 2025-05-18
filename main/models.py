5d9faf792a6cffa23a36babc2ac591badb17436e
indra777-wq <indra96936@gmail.com>
2025-05-19 00:32:19 +0500

    Add Customer

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from unittest.mock import mock_open
 
 from django.db import models
 
+
 # Create your models here.
 
 class Category(models.Model):
@@ -15,3 +16,8 @@ class Product(models.Model):
     color = models.CharField(max_length=15)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=20)
+    phone = models.CharField(max_length = 15)
